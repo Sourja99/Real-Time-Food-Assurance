72827638a56dc2661e617466eeb949c09635f8c9
Sourjadip Pramanik <84491010+Sourja99@users.noreply.github.com>
2022-09-26 01:54:16 +0530

    Update IOT_cp.py

--- IOT_cp.py
+++ IOT_cp.py
@@ -1,13 +1,13 @@
-# from pyexpat import model
-# from re import A, X
-# from turtle import ht
-#from matplotlib.ft2font import LOAD_VERTICAL_LAYOUT
+from pyexpat import model
+from re import A, X
+from turtle import ht
+from matplotlib.ft2font import LOAD_VERTICAL_LAYOUT
 import numpy as np
 import pandas as pd
 import streamlit as st
 import webbrowser
 import pickle
-from collections.abc import Iterable
+#from collections.abc import Iterable
 # import scipy.stats as stats
 from gsheetsdb import connect
 
